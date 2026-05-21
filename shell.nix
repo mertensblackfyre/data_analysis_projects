e65404b77d6784e8f8cf140f3915b8a00097c422
mertensnova <72759681+mertensnova@users.noreply.github.com>
2026-05-21 23:51:20 +0300

    New

--- shell.nix
+++ shell.nix
@@ -3,6 +3,8 @@ let
 in
   pkgs.mkShell {
     packages = [
+    pkgs.python313Packages.sympy
+    pkgs.python313Packages.scipy
     pkgs.python313Packages.matplotlib
     pkgs.python313Packages.numpy
     pkgs.python313Packages.pandas
